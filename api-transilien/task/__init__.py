a9f324b3bb3bd7a381cdcd9ddbc70bd627a1b9aa
Nicolas Leclercq <nicolas.leclercq@synchrotron-soleil.fr>
2019-03-05 09:08:25 +0100

    adding task support

---
+++ api-transilien/task/__init__.py
@@ -0,0 +1,23 @@
+# ===========================================================================
+#  This file is part of the Flyscan Ecosystem
+#
+#  Copyright 2014-EOT Synchrotron SOLEIL, St.Aubin, France
+#
+#  This is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU Lesser General Public License as published by the Free
+#  Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+#  This is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with This.  If not, see <http://www.gnu.org/licenses/>.
+# ===========================================================================
+
+"""This package provides an active object implementation called Task"""
+
+from fs.utils.task.Message import Message, MessageIsNotWaitable, MessageProcessingTimeout
+from fs.utils.task.Task import Task
